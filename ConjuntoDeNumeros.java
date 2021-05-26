6bb448730919eefb1346d8a167acb72bc8d796eb
Gabriel González Rodríguez <gabrielgr01@hotmail.com>
2021-05-25 23:14:56 -0600

    Update ConjuntoDeNumeros.java

--- ConjuntoDeNumeros.java
+++ ConjuntoDeNumeros.java
@@ -1,4 +1,6 @@
-public static int[] redimencioneArregloInt (int[] arregloOriginal, int elementoAgregar){
+    import javax.swing.JOptionPane;
+
+    public static int[] redimencioneArregloInt (int[] arregloOriginal, int elementoAgregar){
       int[] arregloNuevo = new int[arregloOriginal.length + 1]; // se crea el arreglo nuevo con un espacio más que el original
       for (int i = 0; i < arregloOriginal.length; i++){ //copia los elementos del arreglo original en el arreglo nuevo
         arregloNuevo[i] = arregloOriginal[i];
